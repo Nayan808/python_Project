92eea0226837ed41f2461e27cadf5fd7266cc491
Nayan <nnhore31@gmail.com>
2024-02-03 14:57:48 +0530

    Screen Added Using Tkinter

--- database.py
+++ database.py
@@ -1,51 +1,49 @@
 import sqlite3
 def create_table():
-  conn =sqlite3.connect('Products.db');
-  cursor=conn.cursor();
+  conn =sqlite3.connect('inventory.db')
+  cursor=conn.cursor()
 
-  cursor.execute('''
-       CREATE TABLE IF NOT EXITS Products(
+  cursor.execute('''CREATE TABLE inventory(
                  id TEXT PRIMARY KEY,
                  name TEXT,
                  in_stock INTEGER,
-                 price INTEGER)
-       ''') 
+                 price INTEGER) ''') 
   conn.commit()
   conn.close()
 
-def fetch_products():
-  conn=sqlite3.connect('Products.db')
+def fetch_inventory():
+  conn=sqlite3.connect('inventory.db')
   cursor=conn.cursor()
-  cursor.execute('SELECT * FROM Products')
-  Products=cursor.fetchall()
+  cursor.execute('SELECT * FROM inventory')
+  inventory=cursor.fetchall()
   conn.close()
-  return Products
+  return inventory
 
 def insert_product(id,name,in_stock,price):
-  conn=sqlite3.connect('Products.db');
+  conn=sqlite3.connect('inventory.db')
   cursor=conn.cursor()
-  cursor.execute('INSERT INTO Products(id,name,in_stock,price) VALUES (?,?,?,?)'(id,name,in_stock,price))
+  cursor.execute('INSERT INTO inventory(id,name,in_stock,price) VALUES (?,?,?,?)',(id,name,in_stock,price))
   conn.commit()
   conn.close()
 
 def delete_product(id):
   conn=sqlite3.connect('Product.db')
   cursor=conn.cursor()
-  cursor.execute('DELETE FROM Products WHERE id =?',(id,))
+  cursor.execute('DELETE FROM inventory WHERE id =?',(id,))
   conn.commit()
   conn.close()
 
-def update_product(id):
+def update_product(new_name,new_stock,new_price,id):
   conn=sqlite3.connect('Product.db')
   cursor=conn.cursor()
-  cursor.execute('UPDATE Products SET name=?,in_stock=?,price=? WHERE id=?',(new_name,new_stock,new_price,id))
+  cursor.execute('UPDATE inventory SET name=?,in_stock=?,price=? WHERE id=?',(new_name,new_stock,new_price,id))
   conn.commit()
   conn.close()
 
 def id_exists(id):
   conn=sqlite3.connect('Product.db')
   cursor=conn.cursor()
-  cursor.execute('Select Count(*) From Products WHERE id=?',(id,))
+  cursor.execute('Select Count(*) From inventory WHERE id=?',(id,))
   result=cursor.fetchone()
   conn.close()
   return result[0]>0
--- lab.py
+++ lab.py
@@ -1,16 +1,20 @@
 import customtkinter
-from tkinter import * 
-import tkinter as tkk
+from tkinter import *
+from tkinter import ttk 
+from tkinter import messagebox
+import tkinter as tk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import database
 
 app=customtkinter.CTk()
 app.title('AutoMobile Inventory Management')
-app.geometry('1000*1000')
+app.geometry('900x700')
 app.config(bg='#0A0B0C')
 app.resizable(False,False)
 
 font1=('ARIAL',30,'bold')
 font2=('ARIAL',30,'bold')
 font3=('ARIAL',30,'bold')
+
+app.mainloop()
